52a0e5baefe4f8773353398a2546346a946eaf80
fuxingZhang <502545703@qq.com>
2020-07-28 17:25:26 +0800

    delete test file

--- tar.ts
+++
@@ -1,2 +0,0 @@
-import { tar } from "./mod.ts";
-await tar.uncompress("./zfx2.tar.gz","./dest");
--- tgz.ts
+++
@@ -1,8 +0,0 @@
-import * as tgz from "./tgz/mod.ts";
-
-
-console.time("a")
-
-await tgz.compress("./zfx", "./zfx3.tar.gz");
-await tgz.uncompress("./zfx3.tar.gz", "./zfx3");
-console.timeEnd("a")
