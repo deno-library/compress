bb7e533281446b942f804543a6751e9ead5a192b
张复星 <502545703@qq.com>
2020-10-01 00:05:54 +0800

    update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
-export { Tar, Untar } from "https://deno.land/std@0.68.0/archive/tar.ts";
-export { ensureFile } from "https://deno.land/std@0.68.0/fs/ensure_file.ts";
-export { ensureDir } from "https://deno.land/std@0.68.0/fs/ensure_dir.ts";
-export { EventEmitter } from "https://deno.land/std@0.68.0/node/events.ts";
+export { Tar, Untar } from "https://deno.land/std@0.71.0/archive/tar.ts";
+export { ensureFile } from "https://deno.land/std@0.71.0/fs/ensure_file.ts";
+export { ensureDir } from "https://deno.land/std@0.71.0/fs/ensure_dir.ts";
+export { EventEmitter } from "https://deno.land/std@0.71.0/node/events.ts";
 export { crc32, Crc32Stream } from "https://deno.land/x/crc32@v0.2.0/mod.ts";
-export * as path from "https://deno.land/std@0.68.0/path/mod.ts";
+export * as path from "https://deno.land/std@0.71.0/path/mod.ts";
