1da32555d731a2f1874025bd2cd9fa31f4bf5830
fuxingZhang <502545703@qq.com>
2021-04-04 23:05:09 +0800

    update tgz

--- tgz/mod.ts
+++ tgz/mod.ts
@@ -1,12 +1,16 @@
 import * as tar from "../tar/mod.ts";
-import { gzipFile, gunzipFile } from "../gzip/gzip_file.ts";
+import { gunzipFile, gzipFile } from "../gzip/gzip_file.ts";
 import type { compressInterface } from "../interface.ts";
 import { path } from "../deps.ts";
 
 export async function uncompress(src: string, dest: string): Promise<void> {
   const filename = path.basename(src);
+  const extname = path.extname(filename);
+  const tarFilename = extname === ".tgz"
+    ? filename.slice(0, -3) + "tar"
+    : (extname === ".gz" ? filename.slice(0, -3) : filename);
   const tmpDir = await Deno.makeTempDir();
-  const tmpPath = path.join(tmpDir, filename);
+  const tmpPath = path.join(tmpDir, tarFilename);
   await gunzipFile(src, tmpPath);
   await tar.uncompress(tmpPath, dest);
   await Deno.remove(tmpDir, { recursive: true });
